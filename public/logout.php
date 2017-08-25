971f9c9be43a07aa5f9424168c74c1ffee3051b3
Michael Cabelin <ncabelin@gmail.com>
2017-08-25 13:29:48 -0700

    add session unset destroy

--- public/logout.php
+++ public/logout.php
@@ -2,6 +2,8 @@
 session_start();
 if (isset($_COOKIE[session_name()])) {
   setcookie(session_name(), '', time()-86400, '/');
+  session_unset();
+  session_destroy();
   header('Location: /login.php?message=Logged out');
 }
 include('includes/header.php'); ?>
